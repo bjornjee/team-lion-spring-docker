92029443f19356aaa56757e89f75bb5ddd2e74ed
bjornjee <bjornjee95@gmail.com>
2020-09-11 14:55:41 +0800

    Added MONGODB_URL as environ var

--- python/Server.py
+++ python/Server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 import Scrapper
 import sys
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,11 +11,13 @@ class Stock(Resource):
    def post(self):
       json_data = request.get_json(force=True)
       stock = {
-         "sym": json_data["sym"], 
+         "sym": json_data["symbol"],
          "start": json_data["start"],
          "end": json_data["end"],
-      }  
-      Scrapper.scrapeData('mongodb://localhost:27017/teamlion', stock["sym"], stock["start"], stock["end"])
+      }
+      #Get Mongodb var
+      mongodb_url = 'mongodb://localhost:27017/teamlion' if os.getenv('MONGODB_URL') is None else os.getenv('MONGODB_URL')
+      Scrapper.scrapeData(mongodb_url, stock["sym"], stock["start"], stock["end"])
       return stock, 201
 
 api.add_resource(Stock, "/api/stock", "/api/stock/<int:id>")
